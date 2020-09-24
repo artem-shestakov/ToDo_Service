736b6530f3f623f2794c96f75406ea2d91c13ffd
artem-shestakov <ashestakov@MacBook-Pro-Artem.local>
2020-09-25 01:46:49 +0300

    add ServerSelectionTimeoutError exception for auth route

--- todo/auth/routes.py
+++ todo/auth/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, make_response, jsonify
 from .models import User, UserSchema
 from todo.utils.response import response_with
 from mongoengine.errors import ValidationError, DoesNotExist
+from pymongo.errors import ServerSelectionTimeoutError
 import todo.utils.response_code as response_code
 
 
@@ -15,10 +16,13 @@ auth_blueprint = Blueprint(
 @auth_blueprint.route('/users', methods=['GET'])
 def get_users():
     """Getting all users"""
-    users = User.objects.all()
-    users_schema = UserSchema(many=True, only=['id', 'email', 'first_name', 'last_name'])
-    users = users_schema.dump(users)
-    return response_with(response_code.SUCCESS_200, value={'users': users})
+    try:
+        users = User.objects.all()
+        users_schema = UserSchema(many=True, only=['id', 'email', 'first_name', 'last_name'])
+        users = users_schema.dump(users)
+        return response_with(response_code.SUCCESS_200, value={'users': users})
+    except ServerSelectionTimeoutError as err:
+        return response_with(response_code.SERVER_ERROR_500, message=err)
 
 
 @auth_blueprint.route('/user/create', methods=['POST'])
@@ -35,6 +39,8 @@ def create_user():
             return response_with(response_code.SUCCESS_201, value={'user': user})
         except KeyError as err:
             return response_with(response_code.MISSING_PARAMETERS_422, message=err)
+        except ServerSelectionTimeoutError as err:
+            return response_with(response_code.SERVER_ERROR_500, message=err)
     else:
         return response_with(response_code.BAD_REQUEST_400)
 
@@ -48,6 +54,8 @@ def get_user_by_id(user_id):
         return response_with(response_code.BAD_REQUEST_400, message=err)
     except DoesNotExist as err:
         return response_with(response_code.NOT_FOUND_404, message=err)
+    except ServerSelectionTimeoutError as err:
+        return response_with(response_code.SERVER_ERROR_500, message=err)
     user_schema = UserSchema(only=['email', 'first_name', 'last_name'])
     user = user_schema.dump(user)
     return response_with(response_code.SUCCESS_200, value={'user': user})
