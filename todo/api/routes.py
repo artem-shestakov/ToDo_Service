8884a28586bcfff97fa8f4f1b0781bffeb0a6086
artem-shestakov <ashestakov@MacBook-Pro-Artem.local>
2020-09-25 15:32:25 +0300

    Create decorator for catching database exceptions

--- todo/api/routes.py
+++ todo/api/routes.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
 from todo.auth.models import User, UserSchema
 from todo.utils.response import response_with
+from todo.utils.decorators import exception
 import todo.utils.response_code as response_code
-from mongoengine.errors import ValidationError, DoesNotExist
-from pymongo.errors import ServerSelectionTimeoutError
 
 api_blueprint = Blueprint(
     'api',
@@ -13,48 +12,36 @@ api_blueprint = Blueprint(
 
 
 @api_blueprint.route('/user/all', methods=['GET'])
+@exception
 def get_users():
     """Getting all users"""
-    try:
-        users = User.objects.all()
-        users_schema = UserSchema(many=True, only=['id', 'email', 'first_name', 'last_name'])
-        users = users_schema.dump(users)
-        return response_with(response_code.SUCCESS_200, value={'users': users})
-    except ServerSelectionTimeoutError as err:
-        return response_with(response_code.SERVER_ERROR_500, message=err)
+    users = User.objects.all()
+    users_schema = UserSchema(many=True, only=['id', 'email', 'first_name', 'last_name'])
+    users = users_schema.dump(users)
+    return response_with(response_code.SUCCESS_200, value={'users': users})
 
 
 @api_blueprint.route('/user/create', methods=['POST'])
+@exception
 def create_user():
     """Create user by POST request"""
     data = request.get_json()
     if data:
-        try:
-            user = User(email=data['email'], first_name=data['first_name'], last_name=data['last_name'])
-            user.save()
-            # Get this user information for response
-            user_schema = UserSchema(only=['email', 'first_name', 'last_name'])
-            user = user_schema.dump(user)
-            return response_with(response_code.SUCCESS_201, value={'user': user})
-        except KeyError as err:
-            return response_with(response_code.MISSING_PARAMETERS_422, message=err)
-        except ServerSelectionTimeoutError as err:
-            return response_with(response_code.SERVER_ERROR_500, message=err)
+        user = User(email=data['email'], first_name=data['first_name'], last_name=data['last_name'])
+        user.save()
+        # Get this user information for response
+        user_schema = UserSchema(only=['email', 'first_name', 'last_name'])
+        user = user_schema.dump(user)
+        return response_with(response_code.SUCCESS_201, value={'user': user})
     else:
         return response_with(response_code.BAD_REQUEST_400)
 
 
 @api_blueprint.route('/user/<user_id>', methods=['GET'])
+@exception
 def get_user_by_id(user_id):
     """Getting user's info by ID"""
-    try:
-        user = User.objects(id=user_id).get()
-        user_schema = UserSchema(only=['email', 'first_name', 'last_name'])
-        user = user_schema.dump(user)
-        return response_with(response_code.SUCCESS_200, value={'user': user})
-    except ValidationError as err:
-        return response_with(response_code.BAD_REQUEST_400, message=err)
-    except DoesNotExist as err:
-        return response_with(response_code.NOT_FOUND_404, message=err)
-    except ServerSelectionTimeoutError as err:
-        return response_with(response_code.SERVER_ERROR_500, message=err)
+    user = User.objects(id=user_id).get()
+    user_schema = UserSchema(only=['email', 'first_name', 'last_name'])
+    user = user_schema.dump(user)
+    return response_with(response_code.SUCCESS_200, value={'user': user})
