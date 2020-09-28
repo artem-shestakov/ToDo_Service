04ca301ed71ed4dbc1ca1d98947ebb3644a2c222
artem-shestakov <ashestakov@MacBook-Pro-Artem.local>
2020-09-28 19:01:56 +0300

    Add List class and methods for create and get lists

--- todo/api/routes.py
+++ todo/api/routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from todo.auth.models import User, UserSchema, Role
-from todo.board.models import Board, BoardSchema
+from todo.board.models import Board, BoardSchema, List, ListSchema
 from todo.api.utils.response import response_with
 from todo.api.utils.decorators import exception, has_role
 import todo.api.utils.response_code as response_code
 
+# Blueprint init
 api_blueprint = Blueprint(
     'api',
     __name__,
@@ -13,6 +14,7 @@ api_blueprint = Blueprint(
 )
 
 
+# ### User API ###
 @api_blueprint.route('/user', methods=['GET'])
 @exception
 @jwt_required
@@ -99,6 +101,7 @@ def delete_user(user_id):
     return response_with(response_code.SUCCESS_201)
 
 
+# ### Board API ###
 @api_blueprint.route('/board', methods=['GET'])
 @exception
 @jwt_required
@@ -106,7 +109,7 @@ def get_all_boards():
     """Get all user's boards"""
     user = User.objects(email=get_jwt_identity()).get()
     boards = Board.objects(user=user).all()
-    board_schema = BoardSchema(many=True, only=['id', 'title'])
+    board_schema = BoardSchema(many=True, only=['id', 'title', 'lists'])
     boards = board_schema.dump(boards)
     return response_with(response_code.SUCCESS_200, value={'boards': boards})
 
@@ -126,11 +129,12 @@ def get_board_by_id(board_id):
 
 @api_blueprint.route('/board', methods=['POST'])
 @exception
+@jwt_required
 def create_board():
     """Create board"""
     data = request.get_json()
     if data:
-        user = User.objects(id=data['user_id']).get()
+        user = User.objects(email=get_jwt_identity()).get()
         board = Board(title=data['title'], user=user)
         board.save()
         board_schema = BoardSchema(only=['id', 'title', 'user'])
@@ -169,3 +173,39 @@ def delete_board(board_id):
     board = Board.objects(id=board_id).get()
     board.delete()
     return response_with(response_code.SUCCESS_201)
+
+
+# ### List API ###
+@api_blueprint.route('/lists/<id>', methods=['GET'])
+@exception
+@jwt_required
+def get_all_lists(id):
+    """
+    Get all lists's
+    :param id: List ID
+    """
+    getted_list = List.objects(id=id).get()
+    list_schema = ListSchema()
+    getted_list = list_schema.dump(getted_list)
+    return response_with(response_code.SUCCESS_200, value={'list': getted_list})
+
+
+@api_blueprint.route('/lists', methods=['POST'])
+@exception
+@jwt_required
+def create_list():
+    """
+    Create list of board
+    """
+    data = request.get_json()
+    if data:
+        new_list = List(title=data['title'])
+        new_list.save()
+        board = Board.objects(id=data['id_board']).get()
+        board.lists.append(new_list)
+        board.save()
+        list_schema = ListSchema(only=['title'])
+        new_list = list_schema.dump(new_list)
+        return response_with(response_code.SUCCESS_201, value={'list': new_list})
+    else:
+        return response_with(response_code.BAD_REQUEST_400, message='Could not get JSON or JSON empty')
